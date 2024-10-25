1668cce11656debfddd8ed97ecb025817743b330
Luka Markasovic <luka.markasovic@gmail.com>
2024-10-25 22:41:47 +0200

    add data seed

--- API/Program.cs
+++ API/Program.cs
@@ -34,4 +34,18 @@ if (app.Environment.IsDevelopment())
 
 app.MapControllers();
 
+try
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+    throw;
+}
+
 app.Run();
