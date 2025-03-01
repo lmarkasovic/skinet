a50aca6c4f6addbc5cda223423215986b6a9a187
Luka Markasovic <luka.markasovic@gmail.com>
2025-03-01 21:49:47 +0100

    add service to component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
 import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
+import { ShopService } from './core/services/shop.service';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +13,14 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  baseUrl = 'https://localhost:5001/api/';
-  private http = inject(HttpClient)
+  private shopService = inject(ShopService);
   title = 'Skinet';
-  products: any[] = [];
+  products: Product[] = [];
 
   ngOnInit(): void {
-    this.http.get<any>(this.baseUrl + 'products').subscribe({
+    this.shopService.getProducts().subscribe({
       next: response => this.products = response.data,
-      error: error => console.log(error),
-      complete: () => console.log('complete')
+      error: error => console.log(error)
     })
   }
 }
