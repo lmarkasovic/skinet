99dbc44601c775ccc583c3c7d72b03e567d0d747
Luka Markasovic <luka.markasovic@gmail.com>
2025-03-16 22:58:50 +0100

    beautify products page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,26 +1,14 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
-import { HttpClient } from '@angular/common/http';
-import { Product } from './shared/models/product';
-import { Pagination } from './shared/models/pagination';
-import { ShopService } from './core/services/shop.service';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  private shopService = inject(ShopService);
+export class AppComponent {
   title = 'Skinet';
-  products: Product[] = [];
-
-  ngOnInit(): void {
-    this.shopService.getProducts().subscribe({
-      next: response => this.products = response.data,
-      error: error => console.log(error)
-    })
-  }
 }
